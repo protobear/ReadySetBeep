7e935ec43d6a28857b3e5336132c9b7d2d5f61ee
Marnick <43970498+protobear@users.noreply.github.com>
2024-11-01 20:26:15 +0100

    Initial Commit

---
+++ ReadySetBeep/ReadySetBeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadySetBeepApp.swift
+//  ReadySetBeep
+//
+//  Created by Marnick De Grave on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadySetBeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReadySetBeepTests/ReadySetBeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadySetBeepTests.swift
+//  ReadySetBeepTests
+//
+//  Created by Marnick De Grave on 01/11/2024.
+//
+
+import Testing
+@testable import ReadySetBeep
+
+struct ReadySetBeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
